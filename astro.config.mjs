dc7234f756dbca26aee6f0de50751b55ecfd1093
Yevhen Bondarenko <bondarenko.revieews2025o@gmail.com>
2026-05-12 22:09:11 +0200

    fix: remove video dynamic route issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,28 @@ export default defineConfig({
     "/empfehlungen/netzwerk/telekom-speedport-pro-plus-gaming-edition/":
       "/empfehlungen/netzwerk/telekom-speedport-pro-plus-gaming/",
     "/empfehlungen/netzwerk/telekom-speedport-7-wifi7-router/":
-      "/empfehlungen/netzwerk/speedport-7-wifi7-router/",
+    "/empfehlungen/netzwerk/speedport-7-wifi7-router/",
+    "/empfehlungen/sicherheit/sonoff-cam-slim-gen2/":
+    "/empfehlungen/sicherheit/sonoff-cam-slim-gen2/",
+
+    "/empfehlungen/klima/tado-thermostat-x-matter/":
+    "/empfehlungen/klima/tado-thermostat-x-matter/",
+
+    "/empfehlungen/sicherheit/sonoff-cam-slim-gen2/":
+    "/empfehlungen/sicherheit/sonoff-cam-slim-gen2-test/",
+
+    "/empfehlungen/klima/tado-thermostat-x-matter/":
+    "/empfehlungen/klima/tado-thermostat-x/",
+
+    "/empfehlungen/marderschreck-auto":
+    "/empfehlungen/sonstiges/marderschreck-auto/",
+
+
+    "/vergleiche/ueberwachungskamera-test/":
+    "/vergleiche/ueberwachungskameras-2026/",
+
+    "/vergleiche/powerline-oder-wlan-repeater/":
+    "/powerline-oder-mesh-wlan/",
   },
 
   integrations: [
